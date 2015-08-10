ccec704f39b622b1b3423319e801210ed92b7bf3
piglikeyoung <316046510@qq.com>
2015-08-10 09:43:24 +0800

    Initial Commit

---
+++ iOSDevTips/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDevTips
+//
+//  Created by piglikeyoung on 15/8/10.
+//  Copyright (c) 2015年 piglikeyoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDevTips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDevTips
+//
+//  Created by piglikeyoung on 15/8/10.
+//  Copyright (c) 2015年 piglikeyoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
